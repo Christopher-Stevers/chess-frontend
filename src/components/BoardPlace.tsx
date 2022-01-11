9952a43c0dce4362debb9cc9fade44af4ad12a18
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2022-01-10 23:02:24 -0500

    satsify eslint

--- src/components/BoardPlace.tsx
+++ src/components/BoardPlace.tsx
@@ -1,8 +1,6 @@
 import React, { FC, useState }
     from "react";
 import { connect,  } from 'react-redux';
-import { RouteComponentProps } from 'react-router';
-import { ApplicationState } from '../store';
 import * as BoardStore from '../store/Board';
 
 import "./BoardPlace.css";
@@ -22,16 +20,11 @@ const BoardPlace: FC<props> = ({ title, index, outerIndex, showAvailable, moveTo
         }
        else showAvailable(index, outerIndex);
 
-    }
-    const [hidden, setDisplay] = useState(false);
-    function toggleVisibility() {
-        hidden === true ? setDisplay(false)
-            : setDisplay(true);
-    }
+    }/*
     function toLetter(input: number) {
         const arr = ["h", "g", "f", "e", "d", "c", "b", "a"];
         return arr[input];
-    }
+    }*/
     return (
         <div onClick={ handlePlaceClick} className={(title.available==="yes")?"red":((outerIndex+index)%2===0)? "black": "white"}>
           { (title.piece) &&<img className="pieceImg" src={`chessPieces/${title.color+title.piece}.png`}></img>}
